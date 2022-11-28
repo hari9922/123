f1ddee7e4587f39408cdae04c9a3438bd4c49dcc
hari krishna <110917236+hari9922@users.noreply.github.com>
2022-11-28 19:27:57 +0530

    Create apppy.py

---
+++ apppy.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restx import Api, Resource
+app = Flask(__name__)
+api = Api(app)
+
+
+class apitets(Resource):
+  def get(self):
+    return "application deployed"
+  
+  api.add_resource(apitest, '/apitest')
+  
+  if __name__== '__main__':
+    app.run(host='0.0.0.0', port='8080', debug=True)
