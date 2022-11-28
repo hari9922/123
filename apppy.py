a90e5705b05f53bbd82c0ffcf38d42397507623c
hari krishna <110917236+hari9922@users.noreply.github.com>
2022-11-28 19:32:37 +0530

    Update apppy.py

--- apppy.py
+++ apppy.py
@@ -8,7 +8,7 @@ class apitest(Resource):
   def get(self):
     return "application deployed"
   
-  api.add_resource(apitest, '/apitest')
+api.add_resource(apitest, '/apitest')
   
   if __name__== '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
